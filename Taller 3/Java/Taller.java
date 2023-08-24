4b4267e94925ca8f5f2bc11e88dc9a3bfa5baf56
Juan Carlos Largo <134282467+imlargo@users.noreply.github.com>
2023-08-24 17:06:35 -0500

    Update Taller.java

--- Taller 3/Java/Taller.java
+++ Taller 3/Java/Taller.java
@@ -1,13 +1,11 @@
-
-
 // --- Parte 1 ---
 package objtaller3;
-import compras.OrdenCompra;
+import compras.*;
+
 import gestionHumana.Empleado;
 import java.util.ArrayList;
 
 public class ObjTaller3 {
-
     public static void main(String[] args) {
         Producto p1 = new Producto(1, "Escoba", "Aseo");
         Producto p2 = new Producto(2, "Camisa", "Ropa");
@@ -18,14 +16,14 @@ public class ObjTaller3 {
         ArrayList<Producto> productos1 = new ArrayList<>();
         productos1.add(p1);
         productos1.add(p3);
-
         OrdenCompra orden1 = new OrdenCompra(101, "Aseo", emp1, productos1);
         System.out.println(Producto.getTotalProductosPedidos());
         orden1.agregarProducto(p4);
         System.out.println(Producto.getTotalProductosPedidos());
-        orden1..agregarProducto(p5);
+        orden1.agregarProducto(p5);
         System.out.println(Producto.getTotalProductosPedidos());
         System.out.println("Orden " + orden1.codigo + " creada");   
+        
         Empleado emp2 = new Empleado(128,"Susana", "Administradora de sucursal");
         ArrayList<Producto> productos2 = new ArrayList<>();
         productos2.add(p2);
@@ -41,6 +39,7 @@ public class ObjTaller3 {
 }
 
 // --- Parte 2 ---
+
 package gestionHumana;
 
 public class Empleado {
@@ -64,14 +63,14 @@ public class Empleado {
 
 // --- Parte 3 ---
 package compras;
-import **
+import gestionHumana.Empleado;
 import java.util.ArrayList;
 
-public class ** {
-    ** int codigo;
-    ** String tipo;
-    ** Empleado comprador;
-    ** ArrayList<Producto> productos;
+public class OrdenCompra {
+    public int codigo;
+    private String tipo;
+    private Empleado comprador;
+    private ArrayList<Producto> productos;
 
     public OrdenCompra(int codigo, String tipo, Empleado comprador, ArrayList<Producto> productos) {
             this.codigo = codigo;
@@ -90,15 +89,14 @@ public class ** {
     
     public void retirarProducto(Empleado empleado, Producto producto) {
         if (!empleado.tengoPermiso()) {
-            **
+            return;
         }
-
         retirar(producto);
     }
 
 
     private void retirar(Producto producto) {
-        for (int i = 0; i < **; i++) {
+        for (int i = 0; i < productos.size(); i++) {
             if (producto.getCodigo() == productos.get(i).getCodigo()) {
                 productos.remove(i);
                 producto.totalProductosPedidos--;
@@ -116,15 +114,15 @@ public class ** {
 }
 
 
-
 // --- Parte 4 ---
+
 package compras;
 
 public class Producto {
-    ** final int codigo;
-    ** String nombre;
-    ** String tipo;
-    ** static int totalProductosPedidos;
+    private final int codigo;
+    private String nombre;
+    public String tipo;
+    public static int totalProductosPedidos;
     
     public Producto(int codigo, String nombre, String tipo) {
         this.codigo = codigo;
@@ -137,14 +135,14 @@ public class Producto {
     }
 
     public void setCodigo(int codigo) {
-        this.codigo = codigo;
+    	return;
     }
     
     public int getCodigo() {
         return codigo;
     }
     
-    ** int getTotalProductosPedidos() {
+    public static int getTotalProductosPedidos() {
         return totalProductosPedidos;
     }
 }
